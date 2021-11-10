e239591833fd0393a886833e7af4cb900f4d9a0e
left024 <left024@gmail.com>
2021-11-10 13:44:18 +0800

    新增 Cookies 检查

--- bili.sh
+++ bili.sh
@@ -12,7 +12,6 @@ videoLocation="/root/Bilibili/"
 #邮件地址
 mailAddress=""
 
-
 #抓取rss更新
 content=$(wget $rssURL -q -O -)
 #获得时间戳
@@ -23,12 +22,12 @@ content1=${content#*<item>}
 subname=${content1#*\[CDATA\[}
 name=${subname%%\]\]>*}
 #如果时间戳记录文本不存在则创建（此处文件地址自行修改）
-if [ ! -f "${scriptLocation}date.txt" ];then
-    echo 313340 > "$scriptLocation"date.txt
+if [ ! -f "${scriptLocation}date.txt" ]; then
+    echo 313340 >"$scriptLocation"date.txt
 fi
 #如果标题记录文本不存在则创建
-if [ ! -f "${scriptLocation}title.txt" ];then
-    echo 313340 > "${scriptLocation}"title.txt
+if [ ! -f "${scriptLocation}title.txt" ]; then
+    echo 313340 >"${scriptLocation}"title.txt
 fi
 #获得之前下载过的视频标题
 oldtitle=$(cat "${scriptLocation}"title.txt)
@@ -41,128 +40,131 @@ result=$(echo $pubdate | grep "GMT")
 result5=$(echo $oldtitle | grep "$name")
 #echo $result
 #判断当前时间戳和上次记录是否相同，不同则代表收藏列表更新
-if [ "$pubdate" != "$olddate" ] && [ "$result" != "" ] && [ "$result5" = "" ];then
-    #清空 Bilibili 文件夹
-    rm -rf "$videoLocation"*
-    #获得视频下载链接
-    sublink=${subpubdate#*<link>}
-    link=${sublink%%</link>*}
-    av=${link#*video/}
-    #获得封面图下载链接和文件名称
-    subcontent=${content#*<img src=\"}
-    photolink=${subcontent%%\"*}
-    pname=${photolink#*archive/}
-    #下载封面图（图片存储位置应和视频一致）
-    wget -P "$videoLocation$name" $photolink
-    #记录时间戳
-    echo $pubdate > "${scriptLocation}"date.txt
-    #记录标题
-    echo $name >> "${scriptLocation}"title.txt
-    #获取视频清晰度以及大小信息
-    stat=$($you -i -l -c /root/cookies.txt $link)
-    #有几P视频
-    count=$(echo $stat | awk -F'title' '{print NF-1}')
-    #echo $count
-    for((i=0;i<$count;i++));
-    do
-        stat=${stat#*title:}
-        title=${stat%%streams:*}
-        substat=${stat#*quality:}
-        data=${substat%%#*}
-        quality=${data%%size*}
-        size=${data#*size:}
-        title=`echo $title`
-        quality=`echo $quality`
-        size=`echo $size`
-        #每一P的视频标题，清晰度，大小，发邮件用于检查下载是否正确进行
-        #message=${message}"Title: "${title}$'\n'"Quality: "${quality}$'\n'"Size: "${size}$'\n\n' #邮件方式
-        message=${message}"Title:%20"${title}"%0AQuality:%20"${quality}"%0ASize:%20"${size}"%0A%0A"  #telegram方式
-    done
-    #发送开始下载邮件（自行修改邮件地址）
-    #echo "$message" | mail -s "BFD：开始下载" $mailAddress
-    curl "https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&parse_mode=html&text=<b>BFD：开始下载</b>%0A%0A$message"
-    #下载视频到指定位置（视频存储位置自行修改；you-get下载B站经常会出错，所以添加了出错重试代码）
-    count=1
-	while true
-        do
-        $you -l -c /root/cookies.txt -o "$videoLocation$name" $link > z.txt #如果是邮件通知，删除 > z.txt
-        if [ $? -eq 0 ]; then
-            #下载完成
-            #重命名封面图
-            result1=$(echo $pname | grep "jpg")
-            if [ "$result1" != "" ];then
-                mv "$videoLocation$name"/$pname "$videoLocation$name"/poster.jpg
-            else
-                mv "$videoLocation$name"/$pname "$videoLocation$name"/poster.png
-            fi
-            #xml转ass && 获取下载完的视频文件信息
-            for file in "$videoLocation$name"/*
-            do
-                if [ "${file##*.}" = "xml" ]
-                then
-                    "${scriptLocation}"DanmakuFactory -o "${file%%.cmt.xml*}".ass -i "$file"
-                    #删除源文件
-                    #rm "$file"
-                elif [ "${file##*.}" = "mp4" ] || [ "${file##*.}" = "flv" ] || [ "${file##*.}" = "mkv" ];
-                then
-                    videoname=${file#*"$name"\/}
-                    videostat=$(du -h "$file")
-                    videosize=${videostat%%\/*}
-                    videosize=$(echo $videosize)
-                    #videomessage=${videomessage}"Title: "${videoname}$'\n'"Size: "${videosize}$'\n\n'  #邮件方式
-                    videomessage=${videomessage}"Title:%20"${videoname}"%0ASize:%20"${videosize}"%0A%0A"  #telegram方式
+if [ "$pubdate" != "$olddate" ] && [ "$result" != "" ] && [ "$result5" = "" ]; then
+    #Cookies可用性检查
+    stat=$($you -i -l -c "$scriptLocation"cookies.txt https://www.bilibili.com/video/BV1fK4y1t7hj)
+    substat=${stat#*quality:}
+    data=${substat%%#*}
+    quality=${data%%size*}
+    if [[ $quality =~ "4K" ]]; then
+        #清空 Bilibili 文件夹
+        rm -rf "$videoLocation"*
+        #获得视频下载链接
+        sublink=${subpubdate#*<link>}
+        link=${sublink%%</link>*}
+        av=${link#*video/}
+        #获得封面图下载链接和文件名称
+        subcontent=${content#*<img src=\"}
+        photolink=${subcontent%%\"*}
+        pname=${photolink#*archive/}
+        #下载封面图（图片存储位置应和视频一致）
+        wget -P "$videoLocation$name" $photolink
+        #记录时间戳
+        echo $pubdate >"${scriptLocation}"date.txt
+        #记录标题
+        echo $name >>"${scriptLocation}"title.txt
+        #获取视频清晰度以及大小信息
+        stat=$($you -i -l -c "$scriptLocation"cookies.txt $link)
+        #有几P视频
+        count=$(echo $stat | awk -F'title' '{print NF-1}')
+        #echo $count
+        for ((i = 0; i < $count; i++)); do
+            stat=${stat#*title:}
+            title=${stat%%streams:*}
+            substat=${stat#*quality:}
+            data=${substat%%#*}
+            quality=${data%%size*}
+            size=${data#*size:}
+            title=$(echo $title)
+            quality=$(echo $quality)
+            size=$(echo $size)
+            #每一P的视频标题，清晰度，大小，发邮件用于检查下载是否正确进行
+            #message=${message}"Title: "${title}$'\n'"Quality: "${quality}$'\n'"Size: "${size}$'\n\n' #邮件方式
+            message=${message}"Title:%20"${title}"%0AQuality:%20"${quality}"%0ASize:%20"${size}"%0A%0A" #telegram方式
+        done
+        #发送开始下载邮件（自行修改邮件地址）
+        #echo "$message" | mail -s "BFD：开始下载" $mailAddress
+        curl "https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&parse_mode=html&text=<b>BFD：开始下载</b>%0A%0A$message"
+        #下载视频到指定位置（视频存储位置自行修改；you-get下载B站经常会出错，所以添加了出错重试代码）
+        count=1
+        while true; do
+            $you -l -c "$scriptLocation"cookies.txt -o "$videoLocation$name" $link >z.txt #如果是邮件通知，删除 > z.txt
+            if [ $? -eq 0 ]; then
+                #下载完成
+                #重命名封面图
+                result1=$(echo $pname | grep "jpg")
+                if [ "$result1" != "" ]; then
+                    mv "$videoLocation$name"/$pname "$videoLocation$name"/poster.jpg
+                else
+                    mv "$videoLocation$name"/$pname "$videoLocation$name"/poster.png
                 fi
-            done
-            #发送下载完成邮件
-            #echo "$videomessage" | mail -s "BFD：下载完成" $mailAddress
-            curl "https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&parse_mode=html&text=<b>BFD：下载完成</b>%0A%0A$videomessage"
-            #上传至OneDrive 百度云
-            /usr/bin/rclone copy /root/Bilibili OneDrive:1tb/Bilibili
-            /usr/local/bin/BaiduPCS-Go upload /root/Bilibili /
-            #发送通知
-            #echo "$title" | mail -s "BFD：上传完成" $mailAddress #邮件方式
-            curl "https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&parse_mode=html&text=<b>BFD：上传完成</b>%0A%0A$title"
-            break;
-        else
-            if [ "$count" != "1" ];then
-                count=$(($count+1))
-                sleep 2
-            else
-                rm -rf "$videoLocation$name"
+                #xml转ass && 获取下载完的视频文件信息
+                for file in "$videoLocation$name"/*; do
+                    if [ "${file##*.}" = "xml" ]; then
+                        "${scriptLocation}"DanmakuFactory -o "${file%%.cmt.xml*}".ass -i "$file"
+                        #删除源文件
+                        #rm "$file"
+                    elif [ "${file##*.}" = "mp4" ] || [ "${file##*.}" = "flv" ] || [ "${file##*.}" = "mkv" ]; then
+                        videoname=${file#*"$name"\/}
+                        videostat=$(du -h "$file")
+                        videosize=${videostat%%\/*}
+                        videosize=$(echo $videosize)
+                        #videomessage=${videomessage}"Title: "${videoname}$'\n'"Size: "${videosize}$'\n\n'  #邮件方式
+                        videomessage=${videomessage}"Title:%20"${videoname}"%0ASize:%20"${videosize}"%0A%0A" #telegram方式
+                    fi
+                done
+                #发送下载完成邮件
+                #echo "$videomessage" | mail -s "BFD：下载完成" $mailAddress
+                curl "https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&parse_mode=html&text=<b>BFD：下载完成</b>%0A%0A$videomessage"
+                #上传至OneDrive 百度云
+                /usr/bin/rclone copy /root/Bilibili OneDrive:1tb/Bilibili
+                /usr/local/bin/BaiduPCS-Go upload /root/Bilibili /
                 #发送通知
-                #echo "$name" | mail -s "BFD：下载失败" $mailAddress  #邮件
-                curl "https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&parse_mode=html&text=<b>BFD：下载失败</b>"
-                exit
+                #echo "$title" | mail -s "BFD：上传完成" $mailAddress #邮件方式
+                curl "https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&parse_mode=html&text=<b>BFD：上传完成</b>%0A%0A$title"
+                break
+            else
+                if [ "$count" != "1" ]; then
+                    count=$(($count + 1))
+                    sleep 2
+                else
+                    rm -rf "$videoLocation$name"
+                    #发送通知
+                    #echo "$name" | mail -s "BFD：下载失败" $mailAddress  #邮件
+                    curl "https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&parse_mode=html&text=<b>BFD：下载失败</b>"
+                    exit
+                fi
             fi
-        fi
-    done &  #如果是邮件通知，删除 & 和下面的内容(删到wait，fi保留)
+        done & #如果是邮件通知，删除 & 和下面的内容(删到wait，fi保留)
 
-    second="start"
-    secondResult=$(curl "https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&text=$second")
-    subSecondResult="${secondResult#*message_id\":}"
-    messageID=${subSecondResult%%,\"from*}
+        second="start"
+        secondResult=$(curl "https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&text=$second")
+        subSecondResult="${secondResult#*message_id\":}"
+        messageID=${subSecondResult%%,\"from*}
 
-    ccount=0
-    while true
-        do
-        sleep 2
-        text=$(tail -1 z.txt)
-        #subtext="${text#*z.txt}"
-        #subtext=$(echo $subtext)
-        echo $text > zz.txt
-        sed -i -e 's/\r/\n/g' zz.txt
-        text=$(sed -n '$p' zz.txt)
-        result=$(curl -g "https://api.telegram.org/bot$telegram_bot_token/editMessageText?chat_id=$telegram_chat_id&message_id=$messageID&text=$text")
-        aa="{\"ok\":false,\"error_code\":400,\"description\":\"Bad Request: message is not modified: specified new message content and reply markup are exactly the same as a current content and reply markup of the message\"}"
-        bb="{\"ok\":false,\"error_code\":400,\"description\":\"Bad Request: message text is empty\"}"
-        if [ "$result" == "$aa" ] || [ "$result" == "$bb" ]; then
-            #echo "break"
-            if [ "$ccount" != "10" ];then
-		        ccount=$(($ccount+1))
-	        else
-		        break;
-	        fi
-        fi
-    done
-    wait
+        ccount=0
+        while true; do
+            sleep 2
+            text=$(tail -1 z.txt)
+            #subtext="${text#*z.txt}"
+            #subtext=$(echo $subtext)
+            echo $text >zz.txt
+            sed -i -e 's/\r/\n/g' zz.txt
+            text=$(sed -n '$p' zz.txt)
+            result=$(curl -g "https://api.telegram.org/bot$telegram_bot_token/editMessageText?chat_id=$telegram_chat_id&message_id=$messageID&text=$text")
+            aa="{\"ok\":false,\"error_code\":400,\"description\":\"Bad Request: message is not modified: specified new message content and reply markup are exactly the same as a current content and reply markup of the message\"}"
+            bb="{\"ok\":false,\"error_code\":400,\"description\":\"Bad Request: message text is empty\"}"
+            if [ "$result" == "$aa" ] || [ "$result" == "$bb" ]; then
+                #echo "break"
+                if [ "$ccount" != "10" ]; then
+                    ccount=$(($ccount + 1))
+                else
+                    break
+                fi
+            fi
+        done
+        wait
+    else
+        curl "https://api.telegram.org/bot$telegram_bot_token/sendMessage?chat_id=$telegram_chat_id&parse_mode=html&text=<b>BFD：Cookies 文件失效，请更新后重试</b>%0A%0A$videomessage"
+    fi
 fi
